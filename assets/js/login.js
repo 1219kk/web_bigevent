a45fe2a05299284c3132e8fd973eeb839d254dbc
1219kk <1017376279@qq.com>
2022-05-19 14:58:09 +0800

    完成了注册和登录功能

--- assets/js/login.js
+++ assets/js/login.js
@@ -9,4 +9,62 @@ $(function () {
     $(".login-box").show();
     $(".reg-box").hide();
   });
+
+  //获取form
+
+  const form = layui.form;
+
+  //定义表单验证规则
+
+  form.verify({
+    //定义校验密码的规则
+    pwd: [/^[\S]{6,12}$/, "密码必须6到12位，且不能出现空格"],
+    //定义确认密码规则
+    repwd: (val) => {
+      const pwd = $(".reg-box [name=password]").val();
+      if (val !== pwd) return "两次密码不一致！";
+    },
+  });
+
+  //const baseUrl = "http://www.liulongbin.top:3007";
+  //导入layui的弹窗组件 layer
+  const layer = layui.layer;
+  //监听注册表单提交，发送注册请求
+
+  $("#form_reg").on("submit", function (e) {
+    //阻止form默认提交
+    e.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: "/api/reguser",
+      data: {
+        username: $("#form_reg [name=username]").val(),
+        password: $("#form_reg [name=password]").val(),
+      },
+      success: (res) => {
+        if (res.status !== 0) return layer.msg("注册失败");
+        layer.msg("注册成功！");
+        $("#link_login").click();
+      },
+    });
+  });
+
+  //监听登录表单提交
+  $("#form_login").on("submit", function (e) {
+    e.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: "/api/login",
+      data: $(this).serialize(),
+      success: (res) => {
+        if (res.status !== 0) return layer.msg("登录失败");
+        layer.msg("登录成功！");
+        //本地存储
+        // 将登录成功得到的 token 字符串，保存到 localStorage 中
+        localStorage.setItem("token", res.token);
+        // 跳转到主页
+        location.href = "/index.html";
+      },
+    });
+  });
 });
